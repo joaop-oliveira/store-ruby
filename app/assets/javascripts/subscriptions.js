706f16ccf741eff82720b93399605ba79d996a1e
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-05-17 14:23:13 -0300

    showed stripe elemetn on screen

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,3 +1,60 @@
-var public_key = document.querySelector("meta[name='stripe-public-key']").content;
-var stripe = Stripe(public_key);
-var elements = stripe.elements();
+
+document.addEventListener("turbolinks:load", function() {
+    var public_key = document.querySelector("meta[name='stripe-public-key']").content;
+    var stripe = Stripe(public_key);
+    var elements = stripe.elements();
+    var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '18px',
+    width: '500px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+});
